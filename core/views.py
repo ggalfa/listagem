a73d1a35e8c7c6d03bf3a2a796d1853ae05cd34e
ggalfa <gleisoncosta705@gmail.com>
2019-09-12 00:14:07 -0300

    Criando a funcao listar

--- core/views.py
+++ core/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from core.models import Event
+
+
+def list_events(request):
+    # user = request.user
+    event = Event.objects.all()
+    data = {'events': event}
+    return render(request, 'listagem.html', data)
