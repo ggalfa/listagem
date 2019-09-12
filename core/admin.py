9ee950a92b55ec21afc75a22551b305912f67ab4
ggalfa <gleisoncosta705@gmail.com>
2019-09-11 23:28:10 -0300

    Melhorando vizualização dos campos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Event
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_event", "date_create")
+    list_filter = ('usuer', 'date_event',)
+admin.site.register(Event, EventAdmin)
