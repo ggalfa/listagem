549d95f28d065289780aa7e35abf5147e46b6ff7
ggalfa <gleisoncosta705@gmail.com>
2019-09-11 23:25:41 -0300

    Criando a tabela do data base

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Titulo')
+    description = models.TextField(blank=True, null=True)
+    date_event = models.DateTimeField(verbose_name='Data do Evento')
+    date_create = models.DateTimeField(auto_now=True, verbose_name='Criado')
+    usuer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
